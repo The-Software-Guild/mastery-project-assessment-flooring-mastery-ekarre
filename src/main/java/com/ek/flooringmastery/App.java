5776e2d6a1b332758c341b06fe3245a42205dd08
Katya Karre <eik.karre@gmail.com>
2022-11-11 11:58:46 -0600

    updated pom.xml file and added applicationContext.xml files

--- src/main/java/com/ek/flooringmastery/App.java
+++ src/main/java/com/ek/flooringmastery/App.java
@@ -1,4 +1,14 @@
 package com.ek.flooringmastery;
 
+import com.ek.flooringmastery.controller.FlooringController;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        //ApplicationContext appContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        //FlooringController controller = appContext.getBean("controller", FlooringController.class);
+        //controller.run();
+
+    }
 }
